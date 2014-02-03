fbcb52b1c633b2521aab21715750810167bc116d
alandunne13@yahoo.com <alandunne13@yahoo.com>
2014-02-03 19:00:43 +0000

    Added edit-text section to MainActivity

--- src/com/rmp/rmpclient/jsonparsing/MainActivity.java
+++ src/com/rmp/rmpclient/jsonparsing/MainActivity.java
@@ -1,7 +1,5 @@
 package com.rmp.rmpclient.jsonparsing;
 
-import com.rmp.rmpclient.jsonparsing.R;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -36,8 +34,9 @@ public class MainActivity extends ListActivity {
 	private static final String TAG_FIRST_NAME = "firstname";
 	private static final String TAG_LAST_NAME = "lastname";
 	private static final String TAG_PARTY = "party";
-//	private static final String TAG_CONSTITUENCY = "constituency";
-//	private static final String TAG_URL = "url";
+	private static final String TAG_IMAGE = "image";
+	// private static final String TAG_CONSTITUENCY = "constituency";
+	// private static final String TAG_URL = "url";
 	// private static final String TAG_PHONE_MOBILE = "mobile";
 	// private static final String TAG_PHONE_HOME = "home";
 	// private static final String TAG_PHONE_OFFICE = "office";
@@ -45,7 +44,7 @@ public class MainActivity extends ListActivity {
 	protected static final String SHANE_TAG = "SHANE";
 
 	// contacts JSONArray
-//	JSONArray politicians = null;
+	// JSONArray politicians = null;
 
 	// Hashmap for ListView
 	ArrayList<HashMap<String, String>> politicianList;
@@ -63,15 +62,20 @@ public class MainActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
 				// getting values from selected ListItem
-				String firstName = ((TextView) view.findViewById(R.id.firstName)).getText().toString();
+				String firstName = ((TextView) view
+						.findViewById(R.id.firstName)).getText().toString();
 				Log.d(SHANE_TAG, "firstName is : " + firstName);
-				String lastName = ((TextView) view.findViewById(R.id.lastName)).getText().toString();
-				String party = ((TextView) view.findViewById(R.id.party)).getText().toString();
+				String lastName = ((TextView) view.findViewById(R.id.lastName))
+						.getText().toString();
+				String party = ((TextView) view.findViewById(R.id.party))
+						.getText().toString();
 
 				// Starting single contact activity
-				Intent in = new Intent(getApplicationContext(), SinglePoliticianActivity.class);
+				Intent in = new Intent(getApplicationContext(),
+						SinglePoliticianActivity.class);
 				in.putExtra(TAG_FIRST_NAME, firstName);
 				in.putExtra(TAG_LAST_NAME, lastName);
 				in.putExtra(TAG_PARTY, party);
@@ -107,31 +111,33 @@ public class MainActivity extends ListActivity {
 
 			// Making a request to url and getting response
 			String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-			Log.d(SHANE_TAG,"jsonStr: "+jsonStr);
+			Log.d(SHANE_TAG, "jsonStr: " + jsonStr);
 
 			Log.d("Response: ", "> " + jsonStr);
 
 			if (jsonStr != null) {
 				try {
-					//JSONObject jsonObj = new JSONObject(jsonStr);
+					// JSONObject jsonObj = new JSONObject(jsonStr);
 
 					// Getting JSON Array node
-					JSONArray politicians = new JSONArray(jsonStr);// = jsonObj.getJSONArray("");
-					Log.d(SHANE_TAG, "SHANE: Polititcians Length: "+politicians.length());
+					JSONArray politicians = new JSONArray(jsonStr);// =
+																	// jsonObj.getJSONArray("");
+					Log.d(SHANE_TAG, "SHANE: Polititcians Length: "
+							+ politicians.length());
 					// looping through All Contacts
 					for (int i = 0; i < politicians.length(); i++) {
 						JSONObject c = politicians.getJSONObject(i);
 
-//						String id = c.getString(TAG_ID);
+						// String id = c.getString(TAG_ID);
 						String firstName = c.getString(TAG_FIRST_NAME);
-						Log.d(SHANE_TAG,"YAAAAAY firstName: "+firstName);
+						Log.d(SHANE_TAG, "YAAAAAY firstName: " + firstName);
 						String lastName = c.getString(TAG_LAST_NAME);
 						String party = c.getString(TAG_PARTY);
-//						String constituency = c.getString(TAG_CONSTITUENCY);
-//						String url = c.getString(TAG_URL);
+						// String constituency = c.getString(TAG_CONSTITUENCY);
+						// String url = c.getString(TAG_URL);
 
 						// Phone node is JSON Object
-//						JSONObject url = c.getJSONObject(TAG_URL);
+						// JSONObject url = c.getJSONObject(TAG_URL);
 						// String mobile = phone.getString(TAG_PHONE_MOBILE);
 						// String home = phone.getString(TAG_PHONE_HOME);
 						// String office = phone.getString(TAG_PHONE_OFFICE);
@@ -140,13 +146,14 @@ public class MainActivity extends ListActivity {
 						HashMap<String, String> politician = new HashMap<String, String>();
 
 						// adding each child node to HashMap key => value
-//						politician.put(TAG_ID, id);
+						// politician.put(TAG_ID, id);
 						politician.put(TAG_FIRST_NAME, firstName);
 						politician.put(TAG_LAST_NAME, lastName);
 						politician.put(TAG_PARTY, party);
 
 						// adding contact to contact list
 						politicianList.add(politician);
+
 					}
 				} catch (JSONException e) {
 					e.printStackTrace();
@@ -167,10 +174,13 @@ public class MainActivity extends ListActivity {
 			/**
 			 * Updating parsed JSON data into ListView
 			 * */
-			ListAdapter adapter = new SimpleAdapter(MainActivity.this, politicianList, R.layout.list_item, new String[] { TAG_FIRST_NAME, TAG_LAST_NAME, TAG_PARTY }, new int[] {
-					R.id.firstName, R.id.lastName, R.id.party });
-
+			ListAdapter adapter = new SimpleAdapter(MainActivity.this,
+					politicianList, R.layout.list_item,
+					new String[] { TAG_FIRST_NAME, TAG_LAST_NAME, TAG_PARTY,
+							TAG_IMAGE }, new int[] { R.id.firstName,
+							R.id.lastName, R.id.party });
 			setListAdapter(adapter);
+
 		}
 
 	}
