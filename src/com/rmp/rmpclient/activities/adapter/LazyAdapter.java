65376bbfb552737c1f9a059885df03d5e6708a3a
alandunne13@yahoo.com <alandunne13@yahoo.com>
2014-03-31 22:47:32 +0100

    Changed LazyAdapter.

--- src/com/rmp/rmpclient/activities/adapter/LazyAdapter.java
+++ src/com/rmp/rmpclient/activities/adapter/LazyAdapter.java
@@ -17,58 +17,62 @@ import com.squareup.picasso.Picasso;
 
 public class LazyAdapter extends BaseAdapter {
 
-    private final Activity activity;
-    private final PoliticianProfileHandler politicianProfileHandler;
-    private static LayoutInflater inflater = null;
+	private final Activity activity;
+	private final PoliticianProfileHandler politicianProfileHandler;
+	private static LayoutInflater inflater = null;
 
-    public LazyAdapter(final Activity a, final PoliticianProfileHandler politicianProfileHandler) {
-        activity = a;
-        this.politicianProfileHandler = politicianProfileHandler;
-        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+	public LazyAdapter(final Activity a,
+			final PoliticianProfileHandler politicianProfileHandler) {
+		activity = a;
+		this.politicianProfileHandler = politicianProfileHandler;
+		inflater = (LayoutInflater) activity
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
-    @Override
+	@Override
 	public int getCount() {
-        return this.politicianProfileHandler.getSize();
-    }
- 
-    @Override
+		return this.politicianProfileHandler.getSize();
+	}
+
+	@Override
 	public Object getItem(final int position) {
-        return position;
-    }
- 
-    @Override
+		return position;
+	}
+
+	@Override
 	public long getItemId(final int position) {
-        return position;
-    }
- 
-    @Override
-	public View getView(final int position, final View convertView, final ViewGroup parent) {
-        
-    	View vi = convertView;
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, final View convertView,
+			final ViewGroup parent) {
+
+		View vi = convertView;
 		if (convertView == null) {
 			vi = inflater.inflate(R.layout.list_item, null);
 		}
 
-		final TextView	firstName = (TextView) vi.findViewById(R.id.firstName);
-        final TextView lastName = (TextView) vi.findViewById(R.id.lastName);
-        final TextView party = (TextView) vi.findViewById(R.id.party);
-        final TextView id = (TextView) vi.findViewById(R.id.id);
-        final ImageView thumb_image = (ImageView) vi.findViewById(R.id.image);
- 
-        final PoliticianProfile politicianProfile = politicianProfileHandler.getNext();
-        final Politician politician = politicianProfile.getPolitician();
+		final TextView firstName = (TextView) vi.findViewById(R.id.firstName);
+		final TextView lastName = (TextView) vi.findViewById(R.id.lastName);
+		final TextView party = (TextView) vi.findViewById(R.id.party);
+		final TextView id = (TextView) vi.findViewById(R.id.id);
+		final ImageView thumb_image = (ImageView) vi.findViewById(R.id.image);
 
-        firstName.setText(politician.getFirstName());
-        lastName.setText(politician.getLastName());
-        party.setText(politician.getParty());
-        id.setText(politician.getId());
-        Picasso.with(activity)
-        .load(politicianProfile.getImage().getImageUrl())
-        		.resize(140, 160)
-        		.into(thumb_image);
-                
-        return vi;
-    }
-    
+		final PoliticianProfile politicianProfile = politicianProfileHandler
+				.getNext();
+		final Politician politician = politicianProfile.getPolitician();
+
+		firstName.setText(politician.getFirstName());
+		lastName.setText(politician.getLastName());
+		party.setText(politician.getParty());
+		id.setText(politician.getId());
+		final Picasso instance = Picasso.with(activity);
+		instance.setDebugging(true);
+		instance.load(politicianProfile.getImage().getImageUrl())
+				.resize(140, 160).into(thumb_image);
+
+		return vi;
+	}
+
 }
